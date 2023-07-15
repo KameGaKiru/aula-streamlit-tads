626564a22c88e211b887793de96c6b359f8e9200
KameGaKiru <78571286+KameGaKiru@users.noreply.github.com>
2023-07-15 08:32:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go
+from datetime import datetime, timedelta
 
 # Título do app
 st.title('Stock History App')
@@ -16,21 +17,24 @@ data = yf.download(ticker_symbol, start='2020-01-01', end='2023-06-26')
 st.subheader('Histórico')
 st.dataframe(data)
 
-# Exibir o gráfico do símbolo fornecido pelo usuário
-fig1 = go.Figure()
-fig1.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
-fig1.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
-st.plotly_chart(fig1)
+# Exibir o gráfico
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
+fig.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
+st.plotly_chart(fig)
 
 # Baixando os dados das Lojas Americanas
-lojas_americas_data = yf.download("LAME4.SA", start='2020-01-01', end='2023-06-26')
+ticker_symbol_lojas_americas = 'LAME4.SA'
+end_date = datetime.now().strftime("%Y-%m-%d")
+start_date = (datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d")
+data_lojas_americas = yf.download(ticker_symbol_lojas_americas, start=start_date, end=end_date)
 
 # Exibir os dados das Lojas Americanas
 st.subheader('Histórico das Lojas Americanas')
-st.dataframe(lojas_americas_data)
+st.dataframe(data_lojas_americas)
 
 # Exibir o gráfico das Lojas Americanas
-fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=lojas_americas_data.index, y=lojas_americas_data['Close'], name='Fechamento'))
-fig2.update_layout(title="Lojas Americanas", xaxis_title="Data", yaxis_title="Preço")
-st.plotly_chart(fig2)
+fig_lojas_americas = go.Figure()
+fig_lojas_americas.add_trace(go.Scatter(x=data_lojas_americas.index, y=data_lojas_americas['Close'], name='Fechamento'))
+fig_lojas_americas.update_layout(title='Lojas Americanas - Último Ano', xaxis_title='Data', yaxis_title='Preço')
+st.plotly_chart(fig_lojas_americas)
