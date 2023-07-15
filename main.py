15ba97da480fbfeccaf43136dc0b9819cf81f147
KameGaKiru <78571286+KameGaKiru@users.noreply.github.com>
2023-07-15 08:37:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ st.title('Stock History App')
 st.sidebar.title('Selecione o stock')
 ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
 
-# baixando os dados do yahoo finanças
-data = yf.download(ticker_symbol, start='2020-01-01', end = '2023-06-26')
+# Baixando os dados do Yahoo Finanças
+data = yf.download(ticker_symbol, start='2020-01-01', end='2023-06-26')
 
 # Exibir os dados
 st.subheader('Histórico')
@@ -18,6 +18,20 @@ st.dataframe(data)
 
 # Exibir o gráfico
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data.index, y = data['Close'], name = 'Fechamento'))
-fig.update_layout(title = f"{ticker_symbol}", xaxis_title = "Data", yaxis_title = "Preço")
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
+fig.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
 st.plotly_chart(fig)
+
+# Baixando os dados da Petrobras
+ticker_symbol_petrobras = 'PETR4.SA'
+data_petrobras = yf.download(ticker_symbol_petrobras, start='2020-01-01', end='2023-06-26')
+
+# Exibir os dados da Petrobras
+st.subheader('Histórico da Petrobras')
+st.dataframe(data_petrobras)
+
+# Exibir o gráfico da Petrobras
+fig_petrobras = go.Figure()
+fig_petrobras.add_trace(go.Scatter(x=data_petrobras.index, y=data_petrobras['Close'], name='Fechamento'))
+fig_petrobras.update_layout(title='Petrobras', xaxis_title='Data', yaxis_title='Preço')
+st.plotly_chart(fig_petrobras)
