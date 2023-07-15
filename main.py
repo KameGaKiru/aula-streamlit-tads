59cd7ba19180df0799ef4d07d7efead0db9d1355
KameGaKiru <78571286+KameGaKiru@users.noreply.github.com>
2023-07-15 08:43:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
 fig.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
 st.plotly_chart(fig)
 
-# Baixando os dados da Petrobras
-ticker_symbol_petrobras = 'PETR4.SA'
-data_petrobras = yf.download(ticker_symbol_petrobras, start='2020-01-01', end='2023-06-26')
+if ticker_symbol.upper() == 'PETR4':
+    # Baixando os dados da Petrobras
+    data_petrobras = yf.download('PETR4.SA', start='2020-01-01', end='2023-06-26')
 
-# Exibir os dados da Petrobras
-st.subheader('Histórico da Petrobras')
-st.dataframe(data_petrobras)
+    # Exibir os dados da Petrobras
+    st.subheader('Histórico da Petrobras')
+    st.dataframe(data_petrobras)
 
-# Exibir o gráfico da Petrobras
-fig_petrobras = go.Figure()
-fig_petrobras.add_trace(go.Scatter(x=data_petrobras.index, y=data_petrobras['Close'], name='Fechamento'))
-fig_petrobras.update_layout(title='Petrobras', xaxis_title='Data', yaxis_title='Preço')
-st.plotly_chart(fig_petrobras)
+    # Exibir o gráfico da Petrobras
+    fig_petrobras = go.Figure()
+    fig_petrobras.add_trace(go.Scatter(x=data_petrobras.index, y=data_petrobras['Close'], name='Fechamento'))
+    fig_petrobras.update_layout(title='Petrobras', xaxis_title='Data', yaxis_title='Preço')
+    st.plotly_chart(fig_petrobras)
