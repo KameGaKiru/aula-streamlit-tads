4a6772b09342c80a7701d393790ef50bfaee5da7
KameGaKiru <78571286+KameGaKiru@users.noreply.github.com>
2023-07-15 08:33:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go
-from datetime import datetime, timedelta
 
 # Título do app
 st.title('Stock History App')
@@ -10,8 +9,8 @@ st.title('Stock History App')
 st.sidebar.title('Selecione o stock')
 ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
 
-# Baixando os dados do Yahoo Finanças
-data = yf.download(ticker_symbol, start='2020-01-01', end='2023-06-26')
+# baixando os dados do yahoo finanças
+data = yf.download(ticker_symbol, start='2020-01-01', end = '2023-06-26')
 
 # Exibir os dados
 st.subheader('Histórico')
@@ -19,22 +18,6 @@ st.dataframe(data)
 
 # Exibir o gráfico
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
-fig.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
+fig.add_trace(go.Scatter(x=data.index, y = data['Close'], name = 'Fechamento'))
+fig.update_layout(title = f"{ticker_symbol}", xaxis_title = "Data", yaxis_title = "Preço")
 st.plotly_chart(fig)
-
-# Baixando os dados das Lojas Americanas
-ticker_symbol_lojas_americas = 'LAME4.SA'
-end_date = datetime.now().strftime("%Y-%m-%d")
-start_date = (datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d")
-data_lojas_americas = yf.download(ticker_symbol_lojas_americas, start=start_date, end=end_date)
-
-# Exibir os dados das Lojas Americanas
-st.subheader('Histórico das Lojas Americanas')
-st.dataframe(data_lojas_americas)
-
-# Exibir o gráfico das Lojas Americanas
-fig_lojas_americas = go.Figure()
-fig_lojas_americas.add_trace(go.Scatter(x=data_lojas_americas.index, y=data_lojas_americas['Close'], name='Fechamento'))
-fig_lojas_americas.update_layout(title='Lojas Americanas - Último Ano', xaxis_title='Data', yaxis_title='Preço')
-st.plotly_chart(fig_lojas_americas)
