21fe7ad04b5565f93a3026029730339c3c3eb46a
KameGaKiru <78571286+KameGaKiru@users.noreply.github.com>
2023-07-15 08:54:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,16 @@ fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
 fig.update_layout(title=f"{ticker_symbol}", xaxis_title="Data", yaxis_title="Preço")
 st.plotly_chart(fig)
 
-if 'PETROBRAS' in ticker_symbol.upper():
+if ticker_symbol.upper() == 'PETR4':
     # Baixando os dados da Petrobras
-    ticker_symbol_petrobras = 'PETR4.SA'
-    data_petrobras = yf.download(ticker_symbol_petrobras, start='2020-01-01', end='2023-06-26')
+    data_petrobras = yf.download('PETR4.SA', start='2020-01-01', end='2023-06-26')
 
     # Exibir os dados da Petrobras
-    st.sidebar.subheader('Histórico da Petrobras')
-    st.sidebar.dataframe(data_petrobras)
+    st.subheader('Histórico da Petrobras')
+    st.dataframe(data_petrobras)
 
     # Exibir o gráfico da Petrobras
     fig_petrobras = go.Figure()
     fig_petrobras.add_trace(go.Scatter(x=data_petrobras.index, y=data_petrobras['Close'], name='Fechamento'))
     fig_petrobras.update_layout(title='Petrobras', xaxis_title='Data', yaxis_title='Preço')
-    st.sidebar.plotly_chart(fig_petrobras)
+    st.plotly_chart(fig_petrobras)
